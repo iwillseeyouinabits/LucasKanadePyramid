23cd7e44490b060e472cd9648d31a32c3d338ba8
iwillseeyouinabits <isaaclsinger99@gmail.com>
2022-05-18 18:20:39 -0400

    MVP of code

--- LucusPyrimid.py
+++ LucusPyrimid.py
@@ -1,3 +1,5 @@
+import time
+
 import numpy as np
 import cv2 as cv
 import imageio as iio
@@ -33,16 +35,17 @@ def lucus(img1, img2, flowMap):
     hsv[..., 1] = 255
     next = cv.cvtColor(rhs, cv.COLOR_BGR2GRAY)
     flow = cv.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+    # print(flow)
     mag, ang = cv.cartToPolar(flow[..., 0], flow[..., 1])
     hsv[..., 0] = ang * 180 / np.pi / 2
     hsv[..., 2] = cv.normalize(mag, None, 0, 255, cv.NORM_MINMAX)
-    bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
-    gray = cv.cvtColor(bgr, cv.COLOR_BGR2GRAY)
-    cv.imwrite(flowMap, gray)
+    np.save(flowMap, mag)
+    # np.set_printoptions(threshold=np.inf)
+    # np.set_printoptions(suppress=True)
+    # print(np.load(flowMap))
 
 
 def createClowMaps(startInd, endInd):
-    writeIter = 0
     for i in range(startInd, endInd+1):
         num = str(i)
         file1 = str("0"*(6-len(num))) + num
@@ -51,28 +54,40 @@ def createClowMaps(startInd, endInd):
         print(path1)
         print(path2)
         print("______________")
-        lucus(path1, path2, "FlowMap/flowMapV" + str(writeIter) + ".png")
-        writeIter += 1
-        writeIter += 1
+        lucus(path1, path2, "FlowMap/flowMapV" + str(i) + ".npy")
 
-def compare(disp, groundTruth):
-    d = cv.imread(disp)
-    gt = cv.imread(groundTruth)
+def compare(disp, groundTruth, k):
+    d = np.load(disp)
+    gt = np.load(groundTruth)
     num = 0
     den = 0
     for x in range(len(d)):
         for y in range(len(d[x])):
-            pxd = d[x][y][0]
-            if not pxd == 0:
-                pxgt = gt[x][y][0]
-                num += (pxd-pxgt)**2
+            pxd = d[x][y]
+            pxgt = gt[x][y]
+            if not pxd == 0 and not pxgt <= 0:
+                if (pxd - pxgt)**2 > k**2:
+                    num += 1
                 den += 1
+    return num, den
+
+def compareAll(start, end, k):
+    num = 0
+    den = 0
+    for i in range(start, end+1):
+        number = str(i)
+        file1 = str("0"*(6-len(number))) + number
+        gt = "GroundTruth/" + file1 + ".npy"
+        d = "FlowMap/flowMapV" + number + ".npy"
+        tempNum, tempDen = compare(d, gt, k)
+        num += tempNum
+        den += tempDen
+        print(str(i) + "  " + str(num/den))
+    print(num/den)
     return num/den
-
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    createClowMaps(5867, 7480)
+    compareAll(0, 7480, 1)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
